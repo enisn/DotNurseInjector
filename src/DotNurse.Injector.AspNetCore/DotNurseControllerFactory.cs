ecb2ec32a9113b057ca17ce5abcf94a2673ecd62
enisn <iletisim@enisnecipoglu.com>
2020-10-31 01:36:15 +0300

    ✨ Implement DotNurse.Injector.AspNetCore

---
+++ src/DotNurse.Injector.AspNetCore/DotNurseControllerFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNurse.Injector.AspNetCore
+{
+    public class DotNurseControllerFactory : IControllerFactory
+    {
+        private readonly IControllerActivator controllerActivator;
+        private readonly IAttributeInjector attributeInjector;
+
+        public DotNurseControllerFactory(IControllerActivator controllerActivator, IAttributeInjector attributeInjector)
+        {
+            this.controllerActivator = controllerActivator;
+            this.attributeInjector = attributeInjector;
+        }
+
+        public object CreateController(ControllerContext context)
+        {
+            var controller = controllerActivator.Create(context);
+            attributeInjector.InjectIntoMembers(controller, context.HttpContext.RequestServices);
+            return controller;
+        }
+
+        public void ReleaseController(ControllerContext context, object controller)
+        {
+            controllerActivator.Release(context, controller);
+        }
+    }
+}
