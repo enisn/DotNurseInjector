92593fa01f0ea96f38d01b78e5046657e7f80837
enisn <iletisim@enisnecipoglu.com>
2020-11-02 15:43:55 +0300

    ✨ Add InjectServiceAttributeTests

---
+++ tests/DotNurse.Injector.Tests/Environment/NamespaceInjectService/Services.cs
@@ -0,0 +1,46 @@
+﻿using DotNurse.Injector.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNurse.Injector.Tests.Environment.NamespaceInjectService
+{
+
+    public interface IDataProvider<T>
+    {
+        T Get();
+    }
+
+    public interface IMessageDataProvider : IDataProvider<string>
+    {
+    }
+
+    public class MessageDataProvider : IMessageDataProvider
+    {
+        public string Get()
+        {
+            return "Hello World!";
+        }
+    }
+
+    public interface IDataService<T>
+    {
+        T Retrieve();
+    }
+
+    public interface IMessageDataService : IDataService<string>
+    {
+    }
+
+    public class MessageDataService : IMessageDataService
+    {
+        [InjectService]
+        public IMessageDataProvider MessageDataProvider { get; set; }
+        public string Retrieve()
+        {
+            return MessageDataProvider.Get();
+        }
+    }
+}
---
+++ tests/DotNurse.Injector.Tests/InjectServiceAttributeTests.cs
@@ -0,0 +1,54 @@
+﻿using DotNurse.Injector.Tests.Environment.NamespaceInjectService;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotNurse.Injector.Tests
+{
+    public class InjectServiceAttributeTests
+    {
+        [Fact]
+        public void InjectServiceByAttribute_ShouldNotBeNullProperty()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddDotNurseInjector();
+            services.AddServicesFrom("DotNurse.Injector.Tests.Environment.NamespaceInjectService");
+
+            IServiceProvider serviceProvider = new DotNurseServiceProvider(services);
+
+            // Act
+            var service = serviceProvider.GetService<IMessageDataService>();
+
+            // Assert
+            Assert.NotNull(service);
+            Assert.IsType<MessageDataService>(service);
+            var messageDataProvider = service as MessageDataService;
+            Assert.NotNull(messageDataProvider.MessageDataProvider);
+        }
+
+        [Fact]
+        public void InjectServiceByAttribute_ShouldCallInjectedPropertyMethod()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddDotNurseInjector();
+            services.AddServicesFrom("DotNurse.Injector.Tests.Environment.NamespaceInjectService");
+
+            IServiceProvider serviceProvider = new DotNurseServiceProvider(services);
+
+            // Act
+            var service = serviceProvider.GetService<IMessageDataService>();
+
+            var message = service.Retrieve();
+
+            // Assert
+            Assert.NotNull(message);
+            Assert.Equal("Hello World!", message);
+        }
+    }
+}
