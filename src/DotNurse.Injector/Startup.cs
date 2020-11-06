c96e9da3ecb8f2b82888f666c507d048118e3af1
enisn <iletisim@enisnecipoglu.com>
2020-11-06 14:42:35 +0300

    💚 Buid fix

--- src/DotNurse.Injector/Startup.cs
+++ src/DotNurse.Injector/Startup.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DotNurse.Injector
 {
