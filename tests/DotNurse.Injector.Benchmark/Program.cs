496cd4af5931cb9be227709d674fac465d2595c5
enisn <iletisim@enisnecipoglu.com>
2020-12-04 17:00:46 +0300

    🧪 Add benchmark

---
+++ tests/DotNurse.Injector.Benchmark/Program.cs
@@ -0,0 +1,55 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace DotNurse.Injector.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<MyTestStartup>();
+        }
+    }
+
+    [SimpleJob(RuntimeMoniker.HostProcess)]
+    [RPlotExporter]
+    [MemoryDiagnoser]
+    public class MyTestStartup
+    {
+        IServiceCollection services;
+        [GlobalSetup]
+        public void Setup()
+        {
+            services = new ServiceCollection();
+        }
+
+        [Benchmark]
+        public void AddByAttributesWithAssembly()
+        {
+            services.AddServicesByAttributes(assembly: GetType().Assembly);
+        }
+        
+        [Benchmark]
+        public void AddByAttributes()
+        {
+            services.AddServicesByAttributes();
+        }
+
+        [Benchmark]
+        public void AddByNamespace()
+        {
+            services.AddServicesFrom("DotNurse.Injector.Benchmark.Sample");
+        }
+
+        [Benchmark]
+        public void AddByNamespaceWithAssembly()
+        {
+            services.AddServicesFrom("DotNurse.Injector.Benchmark.Sample", configAction: opts => opts.Assembly = GetType().Assembly);
+        }
+    }
+}
---
+++ tests/DotNurse.Injector.Benchmark/Samples/Dependency.cs
@@ -0,0 +1,54 @@
+﻿using DotNurse.Injector.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNurse.Injector.Benchmark.Sample
+{
+    [RegisterAs(typeof(IDependency), ServiceLifetime.Singleton)]
+    [RegisterAs(typeof(IDependencyA))]
+    [RegisterAs(typeof(IAnother), ServiceLifetime.Scoped)]
+    public class DependencyA : IDependencyA
+    {
+    }
+
+    [RegisterAs(typeof(IDependencyB))]
+    public class DependencyB : IDependencyB
+    {
+    }
+
+    public class DependencyC : IDependencyC
+    {
+    }
+
+    public class DependencyD : IDependencyD
+    {
+    }
+
+    public interface IDependencyA : IDependency
+    {
+    }
+
+    public interface IDependencyB : IDependency
+    {
+    }
+
+    public interface IDependencyC : IDependency
+    {
+    }
+
+    public interface IDependencyD : IDependency
+    {
+    }
+
+    public interface IDependency
+    {
+    }
+
+    public interface IAnother
+    {
+    }
+}
