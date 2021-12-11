c431598e346cfc0a5a4cc30d24156c06250ddb3a
enisn <info@enisnecipoglu.com>
2021-12-11 22:22:50 +0300

    ⚡️Performance improvement on FindTypesInNamespace method

--- src/DotNurse.Injector/Services/DotNurseTypeExplorer.cs
+++ src/DotNurse.Injector/Services/DotNurseTypeExplorer.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNurse.Injector.Services;
 
@@ -17,7 +15,7 @@ public class DotNurseTypeExplorer : ITypeExplorer
         if (assembly != null)
             return assembly.GetTypes().Where(x => x.Namespace == @namespace && !x.IsAbstract && x.IsClass);
 
-        var assemblies = AssembliesToSearchFor.Where(x => x.FullName.StartsWith(@namespace.Split('.').FirstOrDefault())).ToList();
+        var assemblies = AssembliesToSearchFor.Where(x => x.FullName.StartsWith(@namespace.Split('.').FirstOrDefault()));
 
         return assemblies.SelectMany(s => s.GetTypes()).Where(x => x.Namespace == @namespace && !x.IsNested && x.IsClass && !x.IsAbstract);
     }
@@ -27,19 +25,18 @@ public class DotNurseTypeExplorer : ITypeExplorer
         if (assembly != null)
             return assembly.GetTypes().Where(x => x.GetCustomAttribute<T>() != null);
 
-        return AssembliesToSearchFor.SelectMany(sm => sm.GetTypes()).Where(x => x.IsDefined(typeof(T)));
+        return FindTypesByExpression(x => x.IsDefined(typeof(T)));
     }
 
     public IEnumerable<Type> FindTypesByExpression(Func<Type, bool> expression, Assembly assembly = null)
     {
-
         if (assembly != null)
             return assembly.GetTypes().Where(expression);
 
         return AssembliesToSearchFor.SelectMany(sm => sm.GetTypes()).Where(expression);
     }
 
-    private static IList<Assembly> AssembliesToSearchFor
+    protected static IList<Assembly> AssembliesToSearchFor
     {
         get
         {
@@ -53,7 +50,7 @@ public class DotNurseTypeExplorer : ITypeExplorer
                             .Select(s => Assembly.Load(s.ToString()))
                             .Concat(new[] { Assembly.GetEntryAssembly() });
 
-                    if (AppDomain.CurrentDomain.FriendlyName == "testhost") // Test host is not referenced directly .dll
+                    if (AppDomain.CurrentDomain.FriendlyName == "testhost") // Test host is not referenced directly as a .dll
                     {
                         _assemblies = _assemblies
                             .Concat(AppDomain.CurrentDomain.GetAssemblies());
